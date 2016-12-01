1f4e6cb4ed63404bc094a19360c017e9bbc45f11
NortonGitHub <yuuki_vs_ninjin@outlook.jp>
2016-12-02 00:20:56 +0900

    [変更] ドルマークの削除

--- Upload2.js
+++ Upload2.js
@@ -1,6 +1,6 @@
 
 
-$(function() {
+(function() {
     $('input[type=file]').after('<span></span>');
 var file;
     // アップロードするファイルを選択
@@ -18,11 +18,13 @@ var file;
         }
 
         // 画像表示
+        /*
         var reader = new FileReader();
         reader.onload = function() {
             var img_src = $('<img>').attr('src', reader.result);
             $('span').html(img_src);
         }
+        */
         reader.readAsDataURL(file);
     });
 });
